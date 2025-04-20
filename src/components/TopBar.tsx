13490ae2e9eb89f067384706c29082c6289395c3
Krishnanand G <118352827+Krishnanand-G@users.noreply.github.com>
2025-04-20 13:15:26 +0530

    New Navbar

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const TopBar = () => {
+  return (
+    <div className="w-full bg-blue-600 text-white text-center py-1 shadow-md">
+      <p className="text-base font-medium">
+        Everything you need to train cutting-edge computer vision models — all in one place.
+      </p>
+    </div>
+  )
+}
+
+export default TopBar
